c15db1045651ac43b42bf9df4ef54a42e923f26b
lunar0x4 <skilled.chair52@gmail.com>
2026-03-30 13:09:14 +0100

    Update loader.lua
    made it easier to see game names or whatever

--- scripts/loader.lua
+++ scripts/loader.lua
@@ -2,20 +2,20 @@
 -- yes, we own all of these.
 
 local scripts = {
-    [13864661000] = "https://oneshotniko-11.pages.dev/breakin2_lobby.lua",
-    [3851622790]  = "https://oneshotniko-11.pages.dev/breakin_lobby.lua",
-    [4620170611]  = "https://oneshotniko-11.pages.dev/breakin_game.lua",
-    [13864667823] = "https://oneshotniko-11.pages.dev/breakin2_game.lua",
-    [5096191125]  = "https://github.com/lunar0x4/game-scripts/raw/refs/heads/main/scripts/Field%20Trip%20Z/hub.lua",
-    [6447798030]  = "https://github.com/lunar0x4/game-scripts/raw/refs/heads/main/scripts/Funky%20Friday/autoplay.lua",
-    [331811267]   = "https://github.com/lunar0x4/game-scripts/raw/refs/heads/main/scripts/Innovation%20INC%20Spaceship/engine_control.lua",
-    [155615604]   = "https://github.com/lunar0x4/game-scripts/raw/refs/heads/main/scripts/Prison%20Life/hub.lua",
-    [2647097243]  = "https://scripts.hageyexpress.xyz/gravitySubmarine.lua",
-    [16454414227] = "https://scripts.hageyexpress.xyz/scary-sushi-c1.lua",
-    [482742811]   = "https://scripts.hageyexpress.xyz/speedingWall.lua",
-    [3245375623]  = "https://scripts.hageyexpress.xyz/ride999999999miles.lua",
-    [11364087119] = "https://scripts.hageyexpress.xyz/cartRide.lua",
-    [510444657]   = "https://rawscripts.net/raw/CRIMINAL-VS.-SWAT-OP-Auto-Farm-40k-an-hour-137938"
+    [13864661000] = "https://oneshotniko-11.pages.dev/breakin2_lobby.lua", -- break in 2 lobby
+    [3851622790]  = "https://oneshotniko-11.pages.dev/breakin_lobby.lua", -- break in 1 lobby
+    [4620170611]  = "https://oneshotniko-11.pages.dev/breakin_game.lua", -- break in 1 game
+    [13864667823] = "https://oneshotniko-11.pages.dev/breakin2_game.lua", -- break in 2 game
+    [5096191125]  = "https://github.com/lunar0x4/game-scripts/raw/refs/heads/main/scripts/Field%20Trip%20Z/hub.lua", -- field trip z
+    [6447798030]  = "https://github.com/lunar0x4/game-scripts/raw/refs/heads/main/scripts/Funky%20Friday/autoplay.lua", -- funky friday
+    [331811267]   = "https://github.com/lunar0x4/game-scripts/raw/refs/heads/main/scripts/Innovation%20INC%20Spaceship/engine_control.lua", --innovation inc spaceship
+    [155615604]   = "https://github.com/lunar0x4/game-scripts/raw/refs/heads/main/scripts/Prison%20Life/hub.lua", -- prison life
+    [2647097243]  = "https://scripts.hageyexpress.xyz/gravitySubmarine.lua", -- gravity submarine
+    [16454414227] = "https://scripts.hageyexpress.xyz/scary-sushi-c1.lua", -- scary sushi (chapter 1)
+    [482742811]   = "https://scripts.hageyexpress.xyz/speedingWall.lua", -- get crushed by a speeding wall
+    [3245375623]  = "https://scripts.hageyexpress.xyz/ride999999999miles.lua", -- ride 999,999,999 miles down a slide or whatever
+    [11364087119] = "https://scripts.hageyexpress.xyz/cartRide.lua", -- cart ride (theres multiple but the correct one is https://www.roblox.com/games/11364087119/Cart-Ride)
+    [510444657]   = "https://rawscripts.net/raw/CRIMINAL-VS.-SWAT-OP-Auto-Farm-40k-an-hour-137938" -- swat vs criminal
 }
 
 local url = scripts[game.PlaceId]
